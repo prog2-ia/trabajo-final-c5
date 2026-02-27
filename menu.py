59814f47c6080462d62d31f3c1481aa169a203ee
enrique-martin <enriquemartinmasegosa1701@gmail.com>
2026-02-27 15:49:04 +0100

    avances menú

---
+++ menu.py
@@ -0,0 +1,89 @@
+from funciones import alta_vehiculo
+
+def inicio():
+    respuesta = ''
+
+    print()
+    print('Bienvenido al sistema'.center(25))
+    print(('-'*25))
+
+    while respuesta != 'cliente' and respuesta != 'empresa':
+
+        print('¿Como quiere acceder?'.center(25))
+        respuesta = input('cliente/empresa: ')
+
+        if respuesta == 'cliente':
+            manu_cliente()
+
+        elif respuesta == 'empresa':
+            menu_empresa()
+
+        else:
+            print()
+            print('ERROR: Acceso Invalido')
+
+def manu_cliente():
+    respuesta = ''
+
+    print(('-'*25))
+
+    while respuesta != '1' and respuesta != '2' and respuesta != '3':
+
+        print('¿Que desea?'.center(25))
+        print('1: Comparar presupuestos')
+        print('2: Alquilar un vehiculo')
+        print('3: Salir')
+
+        respuesta = input('Acción: ')
+        print()
+
+        if respuesta == '1':
+            pass
+
+        elif respuesta == '2':
+            pass
+
+        elif respuesta == '3':
+            print('Saliendo...')
+
+        else:
+            print('ERROR: Acción Invalida')
+
+
+def menu_empresa():
+    respuesta = ''
+
+    print(('-' * 25))
+
+    while respuesta != '1' and respuesta != '2' and respuesta != '3' and respuesta != '4' and respuesta != '5':
+
+        print('¿Que desea?'.center(25))
+        print('1: Dar de alta un vehiculo')
+        print('2: Dar de baja un vehiculo')
+        print('3: Modificar datos de un vehiculo')
+        print('4: Mostrar datos de un vehiculo')
+        print('5: Salir')
+
+        respuesta = input('Acción: ')
+        print()
+
+        if respuesta == '1':
+            alta_vehiculo()
+
+        elif respuesta == '2':
+            pass
+
+        elif respuesta == '3':
+            pass
+
+        elif respuesta == '4':
+            pass
+
+        elif respuesta == '5':
+            print('Saliendo...')
+
+        else:
+            print('ERROR: Acción Invalida')
+
+if __name__ == '__main__':
+    inicio()
